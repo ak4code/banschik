6b08e97537d5afa21c895e2340fafc6b7da383a5
ak4code <haritonov.aka@gmail.com>
2021-01-20 23:48:56 +0300

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <h4>This is test: {{ val }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  props: ['val']
+}
+</script>
+
+<style scoped>
+
+</style>
